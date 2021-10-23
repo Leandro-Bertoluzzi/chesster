922fdaa21d3531bd3d2baa80168c58f603f8cf29
Leandro-Bertoluzzi <leajb@gmail.com>
2021-10-23 01:35:00 -0300

    Updated valid moves for pieces

--- src/widgets/Board.js
+++ src/widgets/Board.js
@@ -49,7 +49,10 @@ class Board extends Component {
             validMoves: [],
             selectedPiece: invalidPiece,
             isSelectedPiece: false,
-            selectedCell: invalidCell
+            selectedCell: invalidCell,
+            boardTable: [],
+            whiteGraveyard: [],
+            blackGraveyard: []
         };
         
         // This is needed to make `this` work in the callback
@@ -59,9 +62,26 @@ class Board extends Component {
     }
 
     componentDidMount() {
+        // We set the first move to 'true' for all pieces
         var piecesInfo = this.state.piecesInfo;
         piecesInfo.forEach((element) => element.firstMove = true);
         this.setState({ piecesInfo });
+
+        // We initialize the board data table
+        var boardTable = this.state.boardTable;
+        for (var i = 0; i < this.props.size; i++) {
+            var cells = [];
+            for(var j = 0; j < this.props.size; j++) {
+                cells.push({ x: j+1, y: i+1, color: "", name: "", firstMove:true });
+            }
+            boardTable.push(cells);
+        }
+        for (i = 0; i < piecesInfo.length; i++) {
+            var x = piecesInfo[i].x - 1;
+            var y = piecesInfo[i].y - 1;
+            boardTable[y][x] = piecesInfo[i];
+        }
+        this.setState({ boardTable });
     }
     
     setSelectedPiece(x, y, color, name, firstMove) {
@@ -131,13 +151,20 @@ class Board extends Component {
 
         var {x, y, color, name, firstMove} = this.state.selectedPiece;
         var validMoves = [];
-        var i = 0;
+        var i, j, k;
 
         // We look for the valid moves for the selected piece
         if(name === "pawn"){
-            validMoves.push({x: x, y: color === "white" ? y+1 : y-1});
-            if(firstMove) {
-                validMoves.push({x: x, y: color === "white" ? y+2 : y-2});
+            let _x = x;
+            let _y = color === "white" ? y+1 : y-1;
+            if (this.state.piecesInfo.findIndex(piece => piece.x === _x && piece.y === _y) === -1) {
+                validMoves.push({x: _x, y: _y});
+                if(firstMove) {
+                    _y = color === "white" ? y+2 : y-2;
+                    if (this.state.piecesInfo.findIndex(piece => piece.x === _x && piece.y === _y) === -1) {
+                        validMoves.push({x: _x, y: _y});
+                    }
+                }
             }
         } else if(name === "knight"){
             validMoves.push({x: x+1, y: y+2});
@@ -149,39 +176,70 @@ class Board extends Component {
             validMoves.push({x: x+2, y: y-1});
             validMoves.push({x: x-2, y: y-1});
         } else if(name === "king"){
-            validMoves.push({x: x, y: y-1});
-            validMoves.push({x: x, y: y+1});
-            validMoves.push({x: x+1, y: y-1});
-            validMoves.push({x: x+1, y: y});
-            validMoves.push({x: x+1, y: y+1});
-            validMoves.push({x: x-1, y: y-1});
-            validMoves.push({x: x-1, y: y});
-            validMoves.push({x: x-1, y: y+1});
+            for(i = -1; i <= 1; i++) {
+                for(j = -1; j <= 1; j++) {
+                    if (!i && !j) { break; }
+                    let _x = x+i*1;
+                    let _y = y+j*1;
+                    validMoves.push({x: _x, y: _y});
+                    if (this.state.piecesInfo.findIndex(piece => piece.x === _x && piece.y === _y) !== -1) {
+                        break;
+                    }
+                }
+            }
         } else if(name === "rook"){
-            for(i = 0; i < this.props.size; i++) {
-                validMoves.push({x: x, y: i+1});
-                validMoves.push({x: i+1, y: y});
+            for(i = -1; i <= 1; i++) {
+                for(j = -1; j <= 1; j++) {
+                    for(k = 1; k < this.props.size; k++) {
+                        if ((!i && !j) || (i && j)) { break; }
+                        let _x = x+i*k;
+                        let _y = y+j*k;
+                        validMoves.push({x: _x, y: _y});
+                        if (this.state.piecesInfo.findIndex(piece => piece.x === _x && piece.y === _y) !== -1) {
+                            break;
+                        }
+                    }
+                }
             }
         } else if(name === "bishop"){
-            for(i = 0; i < this.props.size; i++) {
-                validMoves.push({x: x+i, y: y+i});
-                validMoves.push({x: x+i, y: y-i});
-                validMoves.push({x: x-i, y: y+i});
-                validMoves.push({x: x-i, y: y-i});
+            for(i = -1; i <= 1; i++) {
+                for(j = -1; j <= 1; j++) {
+                    for(k = 1; k < this.props.size; k++) {
+                        if (!i || !j) { break; }
+                        let _x = x+i*k;
+                        let _y = y+j*k;
+                        validMoves.push({x: _x, y: _y});
+                        if (this.state.piecesInfo.findIndex(piece => piece.x === _x && piece.y === _y) !== -1) {
+                            break;
+                        }
+                    }
+                }
             }
         } else if(name === "queen"){
-            for(i = 0; i < this.props.size; i++) {
-                validMoves.push({x: x, y: i+1});
-                validMoves.push({x: i+1, y: y});
-                validMoves.push({x: x+i, y: y+i});
-                validMoves.push({x: x+i, y: y-i});
-                validMoves.push({x: x-i, y: y+i});
-                validMoves.push({x: x-i, y: y-i});
+            for(i = -1; i <= 1; i++) {
+                for(j = -1; j <= 1; j++) {
+                    for(k = 1; k < this.props.size; k++) {
+                        if (!i && !j) { break; }
+                        let _x = x+i*k;
+                        let _y = y+j*k;
+                        validMoves.push({x: _x, y: _y});
+                        if (this.state.piecesInfo.findIndex(piece => piece.x === _x && piece.y === _y) !== -1) {
+                            break;
+                        }
+                    }
+                }
             }
         }
 
         // We filter the moves outside the board
         validMoves = validMoves.filter(cell => cell.x > 0 && cell.x <= this.props.size && cell.y > 0 && cell.y <= this.props.size);
+        // We filter the moves over ally pieces
+        this.state.piecesInfo.forEach(function(piece) {
+            let index = validMoves.findIndex(cell => piece.x === cell.x && piece.y === cell.y && piece.color === color);
+            if (index !== -1) {
+                validMoves.splice(index, 1);
+            }
+        });
 
         // We update validMoves
         this.setState(prevState => {
